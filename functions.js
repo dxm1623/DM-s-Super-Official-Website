76b3eafd72fea9b8ec235422648e8764adfbda99
dxm1623 <160065179+dxm1623@users.noreply.github.com>
2025-02-03 23:17:14 -0500

    Add Webpage Files

---
+++ functions.js
@@ -0,0 +1,11 @@
+function showAboutMe(){
+    document.getElementById("aboutMeContent").style.display = 'block';
+    }
+
+function showBio(){
+    document.getElementById("bioContent").style.display = 'block';
+    }
+
+function showMyWork(){
+    document.getElementById("myWorkContent").style.display = 'block';
+    }
